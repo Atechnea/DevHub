83a64e0a19ea57e8d999166a1ec4e974cbb9f2ca
Beatriz Bueno <Fbuenoalmagro@gmail.com>
2024-03-11 00:49:58 +0100

    act db,js

--- db/db.js
+++ db/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: "dbconndevhub.diskobolo.org",
-    user: "admin_gps",
-    password: "1234",
+    host: "188.127.184.221",
+    user: "devhub_admin",
+    password: "HelloThere123",
     database: "devhub1",
-    port: 3306,
+    port:3306,
     connectionLimit: 50
 });
 
