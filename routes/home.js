e18315bcdf10f8447beedafd93d80e506c066b47
joseatort <joseantoniotortosatic@gmail.com>
2024-03-23 01:57:08 +0100

    Comprobar sesión

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-    res.render('home');
+    if(res.locals.usuario != null) {
+        res.render('home');
+      }
+      else{//NO USUARIO en sesion
+        res.redirect('login');
+      }
 });
 
 
