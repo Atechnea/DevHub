bb6bf0bdfadd74909d348a06c9d0b0f7d71b229c
TenshiZ <114061087+TenshiZ@users.noreply.github.com>
2024-04-02 20:27:04 +0200

    c

--- db/db.js
+++ db/db.js
@@ -13,11 +13,6 @@ const pool = mysql.createPool({
     connectionLimit: 15
 });
 
-<<<<<<< HEAD
-=======
-
-
->>>>>>> origin/develop
 // Configure express-session to use MySQLStore
 const sessionStore = new MySQLStore({
     createDatabaseTable:true
