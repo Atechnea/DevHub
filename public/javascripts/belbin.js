6ad16e4c5d74931f64662bc4264882e99459a15d
joseatort <joseantoniotortosatic@gmail.com>
2024-04-02 17:36:28 +0200

    Revert "Revert "Merge develop""
    This reverts commit 11e38ad7f8e5b1528718dba961c0dce0e1796b05.

---
+++ public/javascripts/belbin.js
@@ -0,0 +1,172 @@
+var seccion = 1;
+var resultados = {"Coordinador": 0, "Impulsor": 0, "Cerebro": 0, "Monitor Evaluador": 0, "Implementador" : 0, "Cohesionador": 0, "Investigador de Recursos": 0, "Finalizador": 0, "Especialista": 0};
+
+$(document).ready(function() {
+    // Function to load the form via AJAX
+    function loadForm(id) {
+      $.ajax({
+        url: "/belbin/form/"+id, // Specify the URL of your form generating endpoint
+        method: "GET",
+        success: function(response) {
+          $("#dynamicFormContainer").html(response); // Load the response into the container
+        },
+        error: function(xhr, status, error) {
+          showToastr('error', 'Ha ocurrido un error', xhr.responseJSON.error);
+        }
+      });
+    }
+
+    function loadResults(data) {
+      $("#dynamicFormContainer").empty();
+      $("#dynamicFormContainer").html("<h3>Estos son tus resultados</h3>" + "<p>Resultado 1: " + data["res1"] + "</p>" + "<p>Resultado 2: " + data["res2"]+ "</p>" + "<p>Resultado 3: " + data["res3"] + "</p>" + '<button class="btn btn-light" id="btn-exit">Listo</button>');
+      $("#btn-exit").on("click", function() {
+        window.location.href = '/';
+      })
+    }
+    // Load the form initially
+    $.ajax({
+      url: '/login/userid',
+      type: 'GET',
+      success: function(res)  {
+        var user_id = res;
+        //show test results
+        $.ajax({
+          url:'/belbin/results/' + user_id,
+          type: 'GET',
+          success: function(res) {
+            if (res) {
+              loadResults(res);
+            } else {
+              loadForm(seccion);
+            }
+          },
+          error: function(error) {
+            showToastr('error', 'Ha ocurrido un error', xhr.responseJSON.error);
+          }
+        })
+      }
+    })
+
+    // Event listener for the arrow icon to dynamically load next form
+    $(document).on("click", ".arrow-icon", function(e) {
+      e.preventDefault();
+      const formData = $("#testform").serializeArray();
+      if (Object.keys(formData).length < 9) {
+        showToastr('error', 'Debe rellenar todas las preguntas', 'Si en alguna pregunta no le has asignado ningún punto, recuerda marcar el botón 0');
+        return;
+      }
+      const sum = formData.reduce(
+        (accumulator, currentValue) => accumulator + parseInt(currentValue.value),
+        0,
+      );
+      if(sum != 10) {
+        showToastr('error', '¡Recuerda!', 'La suma de todas las preguntas de esta sección debe ser 10.');
+        return;
+      }
+      if (seccion == 1) {
+        resultados["Investigador de Recursos"] += parseInt(formData.find((e) => {return e.name == "btn1"}).value);
+        resultados["Cohesionador"] += parseInt(formData.find((e) => {return e.name == "btn2"}).value);
+        resultados["Cerebro"] += parseInt(formData.find((e) => {return e.name == "btn3"}).value);
+        resultados["Coordinador"] += parseInt(formData.find((e) => {return e.name == "btn4"}).value);
+        resultados["Finalizador"] += parseInt(formData.find((e) => {return e.name == "btn5"}).value);
+        resultados["Especialista"] += parseInt(formData.find((e) => {return e.name == "btn6"}).value);
+        resultados["Impulsor"] += parseInt(formData.find((e) => {return e.name == "btn7"}).value);
+        resultados["Implementador"] += parseInt(formData.find((e) => {return e.name == "btn8"}).value);
+        resultados["Monitor Evaluador"] += parseInt(formData.find((e) => {return e.name == "btn9"}).value);
+      } else if (seccion == 2) {
+        resultados["Implementador"] += parseInt(formData.find((e) => {return e.name == "btn1"}).value);
+        resultados["Coordinador"] += parseInt(formData.find((e) => {return e.name == "btn2"}).value);
+        resultados["Especialista"] += parseInt(formData.find((e) => {return e.name == "btn3"}).value);
+        resultados["Investigador de Recursos"] += parseInt(formData.find((e) => {return e.name == "btn4"}).value);
+        resultados["Monitor Evaluador"] += parseInt(formData.find((e) => {return e.name == "btn5"}).value);
+        resultados["Impulsor"] += parseInt(formData.find((e) => {return e.name == "btn6"}).value);
+        resultados["Cohesionador"] += parseInt(formData.find((e) => {return e.name == "btn7"}).value);
+        resultados["Cerebro"] += parseInt(formData.find((e) => {return e.name == "btn8"}).value);
+        resultados["Finalizador"] += parseInt(formData.find((e) => {return e.name == "btn9"}).value);
+      } else if (seccion == 3) {
+        resultados["Coordinador"] += parseInt(formData.find((e) => {return e.name == "btn1"}).value);
+        resultados["Finalizador"] += parseInt(formData.find((e) => {return e.name == "btn2"}).value);
+        resultados["Impulsor"] += parseInt(formData.find((e) => {return e.name == "btn3"}).value);
+        resultados["Cerebro"] += parseInt(formData.find((e) => {return e.name == "btn4"}).value);
+        resultados["Cohesionador"] += parseInt(formData.find((e) => {return e.name == "btn5"}).value);
+        resultados["Investigador de Recursos"] += parseInt(formData.find((e) => {return e.name == "btn6"}).value);
+        resultados["Especialista"] += parseInt(formData.find((e) => {return e.name == "btn7"}).value);
+        resultados["Monitor Evaluador"] += parseInt(formData.find((e) => {return e.name == "btn8"}).value);
+        resultados["Implementador"] += parseInt(formData.find((e) => {return e.name == "btn9"}).value);
+      } else if (seccion == 4) {
+        resultados["Cohesionador"] += parseInt(formData.find((e) => {return e.name == "btn1"}).value);
+        resultados["Especialista"] += parseInt(formData.find((e) => {return e.name == "btn2"}).value);
+        resultados["Impulsor"] += parseInt(formData.find((e) => {return e.name == "btn3"}).value);
+        resultados["Monitor Evaluador"] += parseInt(formData.find((e) => {return e.name == "btn4"}).value);
+        resultados["Implementador"] += parseInt(formData.find((e) => {return e.name == "btn5"}).value);
+        resultados["Cerebro"] += parseInt(formData.find((e) => {return e.name == "btn6"}).value);
+        resultados["Finalizador"] += parseInt(formData.find((e) => {return e.name == "btn7"}).value);
+        resultados["Investigador de Recursos"] += parseInt(formData.find((e) => {return e.name == "btn8"}).value);
+        resultados["Coordinador"] += parseInt(formData.find((e) => {return e.name == "btn9"}).value);
+      } else if (seccion == 5) {
+        resultados["Monitor Evaluador"] += parseInt(formData.find((e) => {return e.name == "btn1"}).value);
+        resultados["Implementador"] += parseInt(formData.find((e) => {return e.name == "btn2"}).value);
+        resultados["Cohesionador"] += parseInt(formData.find((e) => {return e.name == "btn3"}).value);
+        resultados["Impulsor"] += parseInt(formData.find((e) => {return e.name == "btn4"}).value);
+        resultados["Investigador de Recursos"] += parseInt(formData.find((e) => {return e.name == "btn5"}).value);
+        resultados["Coordinador"] += parseInt(formData.find((e) => {return e.name == "btn6"}).value);
+        resultados["Finalizador"] += parseInt(formData.find((e) => {return e.name == "btn7"}).value);
+        resultados["Cerebro"] += parseInt(formData.find((e) => {return e.name == "btn8"}).value);
+        resultados["Especialista"] += parseInt(formData.find((e) => {return e.name == "btn9"}).value);
+      } else if (seccion == 6) {
+        resultados["Especialista"] += parseInt(formData.find((e) => {return e.name == "btn1"}).value);
+        resultados["Cerebro"] += parseInt(formData.find((e) => {return e.name == "btn2"}).value);
+        resultados["Cohesionador"] += parseInt(formData.find((e) => {return e.name == "btn3"}).value);
+        resultados["Coordinador"] += parseInt(formData.find((e) => {return e.name == "btn4"}).value);
+        resultados["Finalizador"] += parseInt(formData.find((e) => {return e.name == "btn5"}).value);
+        resultados["Monitor Evaluador"] += parseInt(formData.find((e) => {return e.name == "btn6"}).value);
+        resultados["Implementador"] += parseInt(formData.find((e) => {return e.name == "btn7"}).value);
+        resultados["Impulsor"] += parseInt(formData.find((e) => {return e.name == "btn8"}).value);
+        resultados["Investigador de Recursos"] += parseInt(formData.find((e) => {return e.name == "btn9"}).value);
+      } else if (seccion == 7) {
+        resultados["Impulsor"] += parseInt(formData.find((e) => {return e.name == "btn1"}).value);
+        resultados["Monitor Evaluador"] += parseInt(formData.find((e) => {return e.name == "btn2"}).value);
+        resultados["Finalizador"] += parseInt(formData.find((e) => {return e.name == "btn3"}).value);
+        resultados["Investigador de Recursos"] += parseInt(formData.find((e) => {return e.name == "btn4"}).value);
+        resultados["Monitor Evaluador"] += parseInt(formData.find((e) => {return e.name == "btn5"}).value);
+        resultados["Cerebro"] += parseInt(formData.find((e) => {return e.name == "btn6"}).value);
+        resultados["Coordinador"] += parseInt(formData.find((e) => {return e.name == "btn7"}).value);
+        resultados["Especialista"] += parseInt(formData.find((e) => {return e.name == "btn8"}).value);
+        resultados["Cohesionador"] += parseInt(formData.find((e) => {return e.name == "btn9"}).value);
+        $.ajax({
+          url:'/belbin/resultsupload',
+          method: "POST",
+          data: resultados,
+          success: function(response) {
+            //get user id
+            var user_id;
+            $.ajax({
+              url: '/login/userid',
+              type: 'GET',
+              success: function(res)  {
+                user_id = res;
+                //show test results
+                $.ajax({
+                  url:'/belbin/results/' + user_id,
+                  type: 'GET',
+                  success: function(res) {
+                    loadResults(res);
+                  },
+                  error: function(error) {
+                    showToastr('error', 'Ha ocurrido un error', xhr.responseJSON.error);
+                  }
+                })
+              }
+            })
+            return;
+          },
+          error: function(error, xhr) {
+            showToastr('error', 'Ha ocurrido un error', xhr.responseJSON.error);
+          }
+        })
+        return;
+      }
+      seccion = seccion + 1;
+      loadForm(seccion);
+    });
+  });
