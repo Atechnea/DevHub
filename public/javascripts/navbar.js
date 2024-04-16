c12d090ea53d78b309aec44e9d3cf3ef01bb224a
joseatort <joseantoniotortosatic@gmail.com>
2024-04-16 23:30:14 +0200

    Progreso actual botón invitaciones

--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -17,3 +17,30 @@ document.getElementById('buscador').addEventListener('click', function() {
 
     }
 });
+
+$(document).ready(function() {
+    // Función para actualizar el número de notificaciones
+
+    console.log('aka');
+    function actualizarNotificaciones() {
+        $.ajax({
+            url: "/home", // Ajusta esta URL a tu backend
+            method: "GET",
+            success: function(data) {
+                // Suponemos que el servidor retorna un objeto JSON con el número de notificaciones
+                $('#num_notificaciones').text(data.num_notificaciones);
+            },
+            error: function() {
+                console.log("Error al obtener las notificaciones.");
+            }
+        });
+    }
+
+    // Establecer intervalo para actualizar las notificaciones cada 10 segundos
+    setInterval(actualizarNotificaciones, 10000); // 10000 milisegundos = 10 segundos
+
+    // También podrías llamar a actualizarNotificaciones() al cargar la página
+    actualizarNotificaciones();
+});
+
+
