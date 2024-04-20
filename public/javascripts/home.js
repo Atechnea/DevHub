127fbae91b49959339be451c76864f69260a0422
joseatort <joseantoniotortosatic@gmail.com>
2024-04-20 15:11:08 +0200

    Toast Hechos

---
+++ public/javascripts/home.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+    // Manejo del clic en el botón de aceptar
+    $("#accept-button").click(function(event)  {
+        event.preventDefault();
+        var invitationId = $(this).data('invitation-id');
+
+        // Envío del formulario por AJAX
+        $.ajax({
+            type: 'post',
+            url: '/home2/rejectInvitation',
+            data: { invitationId: invitationId },
+            success: function(result) {
+                // Si la solicitud es exitosa, mostrar un mensaje de éxito
+                showToastr('success', 'Se ha aceptado la invitación con éxito', );
+            },
+            error: function(xhr, status, error) {
+                // Si hay un error, mostrar un mensaje de error
+                showToastr('error', 'Ha ocurrido un error al aceptar la invitación', xhr.responseJSON.error);
+            }
+        });
+    });
+});
+     
+    $("#reject-button").click(function(event)  {
+        event.preventDefault();
+        var invitationId = $(this).data('invitation-id');
+
+        // Envío del formulario por AJAX
+        $.ajax({
+            type: 'post',
+            url: '/home2/rejectInvitation',
+            data: { invitationId: invitationId },
+            success: function(result) {
+                // Si la solicitud es exitosa, mostrar un mensaje de éxito
+                showToastr('success', 'Se ha rechazado la invitación con éxito', '');
+            },
+            error: function(xhr, status, error) {
+                // Si hay un error, mostrar un mensaje de error
+                showToastr('error', 'Ha ocurrido un error al rechazar la invitación', xhr.responseJSON.error);
+            }
+        });
+
+});
