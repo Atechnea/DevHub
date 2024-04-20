82425a057cc1aa077491133a896bb950762abc79
joseatort <joseantoniotortosatic@gmail.com>
2024-04-20 12:02:39 +0200

    Tarea Mostrar Invitaciones terminada + corrección grande de diseño y responsive

--- app.js
+++ app.js
@@ -16,8 +16,6 @@ var userRouter = require('./routes/users');
 var perfilRouter = require('./routes/perfil');
 var crearEquipoRouter = require('./routes/crearequipo');
 
-var homeRouter2 = require('./routes/home2');
-
 var app = express();
 
 // view engine setup
@@ -85,8 +83,6 @@ app.use('/users', userRouter);
 app.use('/belbin', belbinRouter);
 app.use('/perfil', perfilRouter);
 app.use('/crearequipo', crearEquipoRouter);
-
-app.use('/home2', homeRouter2);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
